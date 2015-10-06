181ba6f761e30dc66c23ad12676e58b5a9b7d2e5
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2015-10-06 19:54:43 +0200

    Fitness cost plot all mutations

--- src/fitness_cost_saturation.py
+++ src/fitness_cost_saturation.py
@@ -7,6 +7,7 @@ content:    Make figure for the mutation rate.
 # Modules
 import os
 import sys
+import argparse
 from itertools import izip
 import numpy as np
 import pandas as pd
@@ -57,35 +58,130 @@ def boot_strap_patients(df, eval_func, columns=None,  n_bootstrap=100):
     return replicates
 
 
-def get_fitness_cost(data, mu='muAbram'):
+def get_fitness_cost(data, mu='muAbram', independent_slope=True):
     '''Get fitness costs from the data'''
-    # Group for the fit
+    # Group by time (binned) and average, then fit nonlinear least squares
     d = (data
          .loc[:, ['time_binc', 'S_binc', mu, 'af']]
          .groupby([mu, 'S_binc', 'time_binc'], as_index=False)
          .mean()
          .groupby('S_binc'))
 
-    # Fit
     from scipy.optimize import curve_fit
-    # x[0] is time, x[1] the mutation rate
-    fun = lambda x, s: x[1] / s * (1 - np.exp(-s * x[0]))
-    s = {}
-    for iS, (S, datum) in enumerate(d):
-        x = np.array(datum[['time_binc', mu]]).T
-        y = np.array(datum['af'])
-        s0 = 1e-2 / 10**(iS / 2.0)
-        fit = curve_fit(fun, x, y, [s0])
-        s[S] = fit[0][0]
+    if not independent_slope:
+        # x[0] is time, x[1] the mutation rate
+        fun = lambda x, s: x[1] / s * (1 - np.exp(-s * x[0]))
+        s = {}
+        for iS, (S, datum) in enumerate(d):
+            x = np.array(datum[['time_binc', mu]]).T
+            y = np.array(datum['af'])
 
-    s = pd.Series(s)
+            ind = np.array(datum[mu] > 1e-5)
+            x = x[:, ind]
+            y = y[ind]
+
+            s0 = 1e-2 / 10**(iS / 2.0)
+            fit = curve_fit(fun, x, y, [s0])
+            s[S] = fit[0][0]
+        s = pd.Series(s)
+    else:
+        # x[0] is time, x[1] the mutation rate
+        fun = lambda x, s1, s2: x[1] / s1 * (1 - np.exp(-s2 * x[0]))
+        s = {}
+        for iS, (S, datum) in enumerate(d):
+            x = np.array(datum[['time_binc', mu]]).T
+            y = np.array(datum['af'])
+
+            ind = np.array(datum[mu] > 1e-5)
+            x = x[:, ind]
+            y = y[ind]
+
+            s0 = 1e-2 / 10**(iS / 2.0)
+            fit = curve_fit(fun, x, y, [s0, s0])
+            s[S] = {'s1': fit[0][0], 's2': fit[0][1]}
+        s = pd.DataFrame(s).T
+
     s.index.name = 'Subtype entropy'
     s.name = 'Fitness cost'
 
     return s
 
 
-def plot_fitness_cost(data, s, mu, ds=None, mut='G->A', savefig=False):
+def plot_fits_allmutations(data, s, mu):
+    '''Plot the fit curves for all mutations'''
+    from matplotlib import cm
+    sns.set_style('darkgrid')
+
+    fig_width = 3
+    fs = 16
+    fig, axs = plt.subplots(4, 4,
+                            figsize=(4 * fig_width, 4 * fig_width))
+
+    for ia1, a1 in enumerate(alphal[:4]):
+        for ia2, a2 in enumerate(alphal[:4]):
+            mut = a1+'->'+a2
+            ax = axs[ia1, ia2]
+            if ia1 == ia2:
+                ax.xaxis.set_ticks([])
+                ax.yaxis.set_ticks([])
+                ax.text(0.5, 0.5, a1, fontsize=fs+6, ha='center', va='center')
+                continue
+
+            # Trajectories
+            d = (data
+                 .groupby('mut')
+                 .get_group(mut)
+                 .loc[:, ['time_binc', 'S_binc', 'af']]
+                 .groupby(['S_binc', 'time_binc'], as_index=False)
+                 .mean()
+                 .groupby('S_binc'))
+            colors = [cm.jet(1.0 * iS / d.ngroups) for iS in xrange(d.ngroups)]
+            for iS, (S, datum) in enumerate(d):
+                x = np.array(datum['time_binc'])
+                y = np.array(datum['af'])
+                ax.scatter(x, y,
+                           s=70,
+                           color=colors[iS],
+                           label='{:1.1G}'.format(S),
+                          )
+            ax.set_ylim(-0.005, 0.08)
+            ax.set_xlim(0, 3000)
+            if (ia1, ia2) != (3, 0):
+                ax.xaxis.set_ticklabels([])
+                ax.yaxis.set_ticklabels([])
+
+            # Fits
+            if isinstance(s, pd.Series):
+                fun = lambda t, s: mu[mut] / s * (1 - np.exp(-s * t))
+                for iS in xrange(d.ngroups):
+                    xfit = np.linspace(0, x.max())
+                    yfit = fun(xfit, s.iloc[iS])
+                    ax.plot(xfit, yfit, lw=2, color=colors[iS], alpha=0.5)
+            else:
+                fun = lambda t, s1, s2: mu[mut] / s1 * (1 - np.exp(-s2 * t))
+                for iS in xrange(d.ngroups):
+                    xfit = np.linspace(0, x.max())
+                    yfit = fun(xfit, s.iloc[iS]['s1'], s.iloc[iS]['s2'])
+                    ax.plot(xfit, yfit, lw=2, color=colors[iS], alpha=0.5)
+
+
+    ax = axs[-1, 0]
+    ax.set_xlabel('Time [days from infection]', fontsize=fs)
+    ax.set_ylabel('Average allele frequency', fontsize=fs)
+    ax.set_xticks(np.linspace(0, 0.08, 5))
+    ax.set_xticks([0, 1000, 2000, 3000])
+    ax.xaxis.set_tick_params(labelsize=fs)
+    ax.yaxis.set_tick_params(labelsize=fs)
+
+    fig.text(0.53, 0.03, 'To', fontsize=20, ha='center', va='center')
+    fig.text(0.03, 0.53, 'From', fontsize=20, ha='center', va='center', rotation=90)
+
+    plt.tight_layout()
+    plt.ion()
+    plt.show()
+
+
+def plot_fitness_cost(data, s, mu, ds=None, mut='A->G', savefig=False):
     '''Plot the fitness costs'''
     from matplotlib import cm
     sns.set_style('darkgrid')
@@ -129,14 +225,23 @@ def plot_fitness_cost(data, s, mu, ds=None, mut='G->A', savefig=False):
             ha='left', va='top')
 
     # Fits
-    fun = lambda t, s: mu[mut] / s * (1 - np.exp(-s * t))
-    for iS in xrange(d.ngroups):
-        xfit = np.linspace(0, x.max())
-        yfit = fun(xfit, s.iloc[iS])
-        ax.plot(xfit, yfit, lw=2, color=colors[iS], alpha=0.5)
-
+    if isinstance(s, pd.Series):
+        fun = lambda t, s: mu[mut] / s * (1 - np.exp(-s * t))
+        for iS in xrange(d.ngroups):
+            xfit = np.linspace(0, x.max())
+            yfit = fun(xfit, s.iloc[iS])
+            ax.plot(xfit, yfit, lw=2, color=colors[iS], alpha=0.5)
+    else:
+        fun = lambda t, s1, s2: mu[mut] / s1 * (1 - np.exp(-s2 * t))
+        for iS in xrange(d.ngroups):
+            xfit = np.linspace(0, x.max())
+            yfit = fun(xfit, s.iloc[iS]['s1'], s.iloc[iS]['s2'])
+            ax.plot(xfit, yfit, lw=2, color=colors[iS], alpha=0.5)
 
     # Costs
+    if not isinstance(s, pd.Series):
+        s = s['s2']
+
     ax = axs[1]
     x = np.array(s.index)
     y = -np.array(s)
@@ -287,10 +392,18 @@ def collect_data(patients, cov_min=100):
 # Script
 if __name__ == '__main__':
 
-    patients = ['p1', 'p2', 'p3','p5', 'p6', 'p8', 'p9', 'p11']
-    cov_min = 100
+    parser = argparse.ArgumentParser(description='Fitness cost')
+    parser.add_argument('--regenerate', action='store_true',
+                        help="regenerate data")
+    args = parser.parse_args()
 
-    data = collect_data(patients, cov_min=cov_min)
+    if args.regenerate:
+        patients = ['p1', 'p2', 'p3','p5', 'p6', 'p8', 'p9', 'p11']
+        cov_min = 100
+        data = collect_data(patients, cov_min=cov_min)
+        data.to_pickle('../data/fitness_cost_data.pickle')
+    else:
+        data = pd.read_pickle('../data/fitness_cost_data.pickle')
 
     t_bins = np.array([0, 500, 1000, 1500, 2000, 3000], int)
     t_binc = 0.5 * (t_bins[:-1] + t_bins[1:])
@@ -303,10 +416,15 @@ if __name__ == '__main__':
     data['S_binc'] = S_binc[data['S_bin']]
 
     s = get_fitness_cost(data)
-    sBS = boot_strap_patients(data, get_fitness_cost, n_bootstrap=100)
-    ds = s.copy()
-    for key, _ in s.iteritems():
-        ds[key] = np.std([tmp[key] for tmp in sBS])
+
+    if False:
+        ds = s.copy()
+        sBS = boot_strap_patients(data, get_fitness_cost, n_bootstrap=100)
+        for key, _ in s.iteritems():
+            ds[key] = np.std([tmp[key] for tmp in sBS])
+    else:
+        ds = None
 
     mu = data.loc[:, ['mut', 'mu']].groupby('mut').mean()['mu']
+    muA = data.loc[:, ['mut', 'muAbram']].groupby('mut').mean()['muAbram']
     plot_fitness_cost(data, s, mu, ds=ds)
