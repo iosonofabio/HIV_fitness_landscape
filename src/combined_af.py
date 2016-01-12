b19c9d271eeea749eab07e2420215148cad77e3d
Richard <richard.neher@tuebingen.mpg.de>
2016-01-12 16:28:59 +0100

    added HLA f\iles

--- src/combined_af.py
+++ src/combined_af.py
@@ -34,18 +34,21 @@ def af_average(afs):
 if __name__=="__main__":
     patients = ['p1', 'p2','p3','p5','p6', 'p8', 'p9','p10', 'p11'] # all subtypes
     #patients = ['p2','p3','p5', 'p8', 'p9','p10', 'p11'] # subtype B only
-    regions = ['pol']
-    subtype='any'
+    regions = ['RRE'] #, 'gag']
+    subtype='B'
+    mut_rate = 2e-5
+    cols = sns.color_palette(n_colors=4)
+    ls = {'gag':'-', 'pol':'--', 'nef':'-.'}
 
     hxb2 = HIVreference(refname='HXB2', subtype = 'B')
     good_pos_in_reference = hxb2.get_ungapped(threshold = 0.05)
     cov_min=1000
     combined_af={}
-    combined_pairing_prob={}
+    combined_phenos={}
     combined_af_by_pat={}
     for region in regions:
         combined_af[region] = np.zeros((6, len(hxb2.annotation[region])))
-        combined_pairing_prob[region] = np.zeros(len(hxb2.annotation[region]))
+        combined_phenos[region] = np.zeros(len(hxb2.annotation[region]))
         combined_af_by_pat[region] = {}
         for pi, pcode in enumerate(patients):
             combined_af_by_pat[region][pcode] = np.zeros((6, len(hxb2.annotation[region])))
@@ -68,7 +71,13 @@ if __name__=="__main__":
                 final = aft[-1].argmax(axis=0)[patient_to_subtype[:,2]]
                 pp = [p.pos_to_feature[pos]['RNA pairing probability'] for pos in p.annotation[region]]
                 pp = np.array(map(lambda x:0.0 if x is None else x, pp))
-                combined_pairing_prob[region][patient_to_subtype[:,0]-patient_to_subtype[0][0]]+= pp[patient_to_subtype[:,2]]
+                combined_phenos[region][patient_to_subtype[:,0]-patient_to_subtype[0][0]]+= pp[patient_to_subtype[:,2]]
+
+                pprob = np.array([p.pos_to_feature[x]['RNA pairing probability']>0.5 for x in p.annotation[region]])
+                plt.figure()
+                print("fraction paired:", pprob.mean())
+                for ci,c in enumerate(p.get_initial_indices(region)):
+                    if pprob[ci]: plt.plot(aft[:,c,ci])
 
                 for af, ysi, depth in izip(aft, p.ysi, p.n_templates_dilutions):
                     if ysi<1:
@@ -124,34 +133,34 @@ if __name__=="__main__":
         plt.xlim([0.0001, 2])
         plt.savefig('figures/'+region+'_entropy_scatter.pdf')
 
-        plt.figure()
-        plt.title(region+' entropy scatter')
-        plt.hexbin(combined_entropy[region][ind]+.00005, xsS[ind]+.012, xscale='log', yscale='log', gridsize=15, cmap = cm.YlOrRd_r, bins='log')
-        plt.ylabel('cross-sectional entropy')
-        plt.xlabel('pooled within patient entropy')
+#        plt.figure()
+#        plt.title(region+' entropy scatter')
+#        plt.hexbin(combined_entropy[region][ind]+.00005, xsS[ind]+.012, xscale='log', yscale='log', gridsize=15, cmap = cm.YlOrRd_r, bins='log')
+#        plt.ylabel('cross-sectional entropy')
+#        plt.xlabel('pooled within patient entropy')
 
 
-        plt.figure()
-        plt.title(region+' number of nuc >0')
-        for ni in range(1,5):
-            ind = (combined_af[region]>0.0001).sum(axis=0)==ni
-            y,x = np.histogram(xsS[ind], bins=np.logspace(-2.7,0.3,21))
-            plt.plot(x[1:], y/np.diff(x)/y.sum(), label=str(ni))
-        plt.legend()
-        plt.xscale('log')
-        plt.yscale('log')
-        plt.savefig('figures/'+region+'_entropy_histogram.pdf')
+#        plt.figure()
+#        plt.title(region+' number of nuc >0')
+#        for ni in range(1,5):
+#            ind = (combined_af[region]>0.0001).sum(axis=0)==ni
+#            y,x = np.histogram(xsS[ind], bins=np.logspace(-2.7,0.3,21))
+#            plt.plot(x[1:], y/np.diff(x)/y.sum(), label=str(ni))
+#        plt.legend()
+#        plt.xscale('log')
+#        plt.yscale('log')
+#        plt.savefig('figures/'+region+'_entropy_histogram.pdf')
 
-        plt.figure()
-        plt.title(region+' number of nuc>0')
-        for ni in range(1,5):
-            ind = (combined_af[region]>0.0001).sum(axis=0)==ni
-            y,x = np.histogram(np.arange(len(ind))[ind]%3, bins=[-0.5, 0.5, 1.5, 2.5])
-            plt.plot(1.0*y/y.sum(), label=str(ni)+' n='+str(np.sum(ind)))
-        plt.legend(loc=2)
-        plt.xlabel('codon position')
-        plt.ylabel('fraction of sites')
-        plt.savefig('figures/'+region+'_non_zero_sites.pdf')
+#        plt.figure()
+#        plt.title(region+' number of nuc>0')
+#        for ni in range(1,5):
+#            ind = (combined_af[region]>0.0001).sum(axis=0)==ni
+#            y,x = np.histogram(np.arange(len(ind))[ind]%3, bins=[-0.5, 0.5, 1.5, 2.5])
+#            plt.plot(1.0*y/y.sum(), label=str(ni)+' n='+str(np.sum(ind)))
+#        plt.legend(loc=2)
+#        plt.xlabel('codon position')
+#        plt.ylabel('fraction of sites')
+#        plt.savefig('figures/'+region+'_non_zero_sites.pdf')
 
         plt.figure()
         plt.title(region+' minor freq')
@@ -183,56 +192,69 @@ if __name__=="__main__":
 
         thres = np.arange(0,1.01, 0.2)
         s_coeff = []
+        S_cutoff = []
         s_counts = []
         for q_low, q_up in zip(thres[:-1], thres[1:]):
             S_low, S_up = scoreatpercentile(xsS, q_low*100), scoreatpercentile(xsS, q_up*100)
             ind = (xsS>=S_low)&(xsS<S_up)
-            tmp_s = [np.median(1e-5/(minor_af[region]+0.0001)[ind])]
+            tmp_s = [np.median(mut_rate/(minor_af[region]+0.0001)[ind])]
+            tmp_Sc = [(S_low+S_up)*0.5]
             tmp_counts = [ind.sum()]
             for ni in range(3):
                 pos_ind = (np.arange(len(xsS))%3)==ni
                 S_low, S_up = scoreatpercentile(xsS[pos_ind], q_low*100), scoreatpercentile(xsS[pos_ind], q_up*100)
                 ind = (xsS[pos_ind]>=S_low)&(xsS[pos_ind]<S_up)
-                tmp_s.append(np.median(1e-5/(minor_af[region][pos_ind][ind]+0.0001)))
+                tmp_s.append(np.median(mut_rate/(minor_af[region][pos_ind][ind]+0.0001)))
+                tmp_Sc.append((S_low+S_up)*0.5)
                 tmp_counts.append(ind.sum())
             s_counts.append(tmp_counts)
             s_coeff.append(tmp_s)
+            S_cutoff.append(tmp_Sc)
             print(q_low, q_up, ":", tmp_s, tmp_counts)
 
-        plt.figure()
+        plt.figure('sel_coeff')
         s_coeff=np.array(s_coeff)
-        plt.plot((thres[1:]+thres[:-1])*0.5, s_coeff)
-
-        ii=2
-        plt.figure()
-        ind = (~(np.isnan(minor_af_part[region][ii])|np.isnan(minor_af_part[region][ii+1])))&\
-              ((minor_af_part[region][ii]>0) & (minor_af_part[region][ii+1]>0))
-        plt.scatter(minor_af_part[region][ii][ind]+1e-6, minor_af_part[region][ii+1][ind]+1e-6) #, yscale='log', xscale='log')
-        print(spearmanr(minor_af_part[region][ii][ind]+1e-6, minor_af_part[region][ii+1][ind]+1e-6))
+        S_cutoff=np.array(S_cutoff)+.005
+        for ii in range(4):
+            label = 'codon '+str(ii) if ii else 'all'
+            plt.plot(S_cutoff[:,ii], s_coeff[:,ii], 'o-',label=region+': '+label, c=cols[ii], ls=ls[region])
         plt.yscale('log')
         plt.xscale('log')
-        plt.xlim([1e-6,1])
-        plt.ylim([1e-6,1])
+        plt.legend(loc=3, numpoints=2)
+        plt.ylabel('fitness cost')
+        plt.xlabel('conservation')
 
-        ii=2
-        plt.figure()
-        plt.hist(np.log10(minor_af_part[region][ii][ind]+1e-6) - np.log10(minor_af_part[region][ii+1][ind]+1e-6), bins=30)
-        print("Difference std:", np.std(np.log10(minor_af_part[region][ii][ind]+1e-6) - np.log10(minor_af_part[region][ii+1][ind]+1e-6)))
-        print("Single std:", np.std(np.log10(minor_af_part[region][ii][ind]+1e-6)), np.std(np.log10(minor_af_part[region][ii+1][ind]+1e-6)))
+        # ii=2
+        # plt.figure()
+        # ind = (~(np.isnan(minor_af_part[region][ii])|np.isnan(minor_af_part[region][ii+1])))&\
+        #       ((minor_af_part[region][ii]>0) & (minor_af_part[region][ii+1]>0))
+        # plt.scatter(minor_af_part[region][ii][ind]+1e-6, minor_af_part[region][ii+1][ind]+1e-6) #, yscale='log', xscale='log')
+        # print(spearmanr(minor_af_part[region][ii][ind]+1e-6, minor_af_part[region][ii+1][ind]+1e-6))
+        # plt.yscale('log')
+        # plt.xscale('log')
+        # plt.xlim([1e-6,1])
+        # plt.ylim([1e-6,1])
+
+        # ii=2
+        # plt.figure()
+        # plt.hist(np.log10(minor_af_part[region][ii][ind]+1e-6) - np.log10(minor_af_part[region][ii+1][ind]+1e-6), bins=30)
+        # print("Difference std:", np.std(np.log10(minor_af_part[region][ii][ind]+1e-6) - np.log10(minor_af_part[region][ii+1][ind]+1e-6)))
+        # print("Single std:", np.std(np.log10(minor_af_part[region][ii][ind]+1e-6)), np.std(np.log10(minor_af_part[region][ii+1][ind]+1e-6)))
 
 
-        plt.figure()
-        minor_af_array=np.array(minor_af_bs[region])
+        # plt.figure()
+        # minor_af_array=np.array(minor_af_bs[region])
 
-        plt.title(region+' min/max frequency of boot strap replicates')
-        minor_af_extrema = np.vstack((minor_af_array.min(axis=0), np.median(minor_af_array, axis=0), minor_af_array.max(axis=0)))
-        sorted_by_median = np.array(sorted(minor_af_extrema.T, key=lambda x:x[1]))
-        plt.plot(sorted_by_median[:,0], label='minimum')
-        plt.plot(sorted_by_median[:,1], label='median')
-        plt.plot(sorted_by_median[:,2], label='maximum')
-        plt.yscale('log')
-        plt.ylabel('frequency')
-        plt.xlabel('positions sorted by median')
-        plt.legend(loc=2)
-        plt.savefig('figures/'+region + '_frequency_noise.pdf')
+        # plt.title(region+' min/max frequency of boot strap replicates')
+        # minor_af_extrema = np.vstack((minor_af_array.min(axis=0), np.median(minor_af_array, axis=0), minor_af_array.max(axis=0)))
+        # sorted_by_median = np.array(sorted(minor_af_extrema.T, key=lambda x:x[1]))
+        # plt.plot(sorted_by_median[:,0], label='minimum')
+        # plt.plot(sorted_by_median[:,1], label='median')
+        # plt.plot(sorted_by_median[:,2], label='maximum')
+        # plt.yscale('log')
+        # plt.ylabel('frequency')
+        # plt.xlabel('positions sorted by median')
+        # plt.legend(loc=2)
+        # plt.savefig('figures/'+region + '_frequency_noise.pdf')
 
+    plt.savefig('figures/selection_coefficients.pdf')
