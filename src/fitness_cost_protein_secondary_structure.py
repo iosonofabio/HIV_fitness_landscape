a3f9ae1799c79f097cbde1c1ebe74b7e3712290f
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2016-01-29 00:01:24 +0100

    Allele freqs VS protein secondary structure

--- src/fitness_cost_protein_secondary_structure.py
+++ src/fitness_cost_protein_secondary_structure.py
@@ -43,10 +43,7 @@ def collect_data(patients, cov_min=100, no_sweeps=False, refname='HXB2'):
         print pcode
 
         p = Patient.load(pcode)
-
-        sec_str = load_secondary_structure_patient(pcode)
-
-        import ipdb; ipdb.set_trace()
+        p.sec_str = load_secondary_structure_patient(pcode)
 
         comap = (pd.DataFrame(p.map_to_external_reference('genomewide', refname=refname)[:, :2],
                               columns=[refname, 'patient'])
@@ -56,6 +53,9 @@ def collect_data(patients, cov_min=100, no_sweeps=False, refname='HXB2'):
         aft = p.get_allele_frequency_trajectories('genomewide', cov_min=cov_min)
         for pos, aft_pos in enumerate(aft.swapaxes(0, 2)):
             fead = p.pos_to_feature[pos]
+
+            # Look at the protein secondary structure
+            sec_str = p.sec_str[pos]
 
             # Keep only sites within ONE protein
             # Note: we could drop this, but then we cannot quite classify syn/nonsyn
@@ -114,6 +114,7 @@ def collect_data(patients, cov_min=100, no_sweeps=False, refname='HXB2'):
                 muA_pos = muA[mut]
 
                 for it, (t, af_nuc, n_temp) in enumerate(izip(times, aft_nuc, n_templates)):
+
                     datum = {'time': t,
                              'af': af_nuc,
                              'pos': pos,
@@ -126,6 +127,7 @@ def collect_data(patients, cov_min=100, no_sweeps=False, refname='HXB2'):
                              'S': S_pos,
                              'syn': syn,
                              'n_templates': n_temp,
+                             'protein_secondary_structure': sec_str,
                             }
                     data.append(datum)
 
@@ -156,3 +158,71 @@ if __name__ == '__main__':
     else:
         data = pd.read_pickle(fn)
 
+    # Make time and entropy bins
+    t_bins = np.array([0, 500, 1000, 1500, 2000, 3000], int)
+    t_binc = 0.5 * (t_bins[:-1] + t_bins[1:])
+    add_binned_column(data, t_bins, 'time')
+    data['time_binc'] = t_binc[data['time_bin']]
+
+    # No-entropy sites are many, so the bin 0 comes up twice...
+    S_bins = np.percentile(data['S'], np.linspace(0, 100, 8))[1:]
+    S_binc = 0.5 * (S_bins[:-1] + S_bins[1:])
+    n_alleles = np.array(data.loc[:, ['af', 'S_bin']].groupby('S_bin').count()['af'])
+    add_binned_column(data, S_bins, 'S')
+    data['S_binc'] = S_binc[data['S_bin']]
+
+    # Average by time bin, syn, and sec structure
+    dav = (data
+           .loc[:, ['time_binc', 'syn', 'protein_secondary_structure', 'af']]
+           .groupby(['time_binc', 'syn', 'protein_secondary_structure'])
+           .mean()
+           .loc[:, 'af']
+           .unstack('time_binc'))
+
+    def average_data(data):
+        dav = (data
+               .copy()
+               .loc[:, ['time_binc', 'syn', 'protein_secondary_structure', 'af']]
+               .groupby(['time_binc', 'syn', 'protein_secondary_structure'])
+               .mean()
+               .loc[:, 'af']
+               #.unstack('time_binc')
+              )
+        return dav
+    from util import boot_strap_patients
+    reps = pd.concat(boot_strap_patients(data, average_data, n_bootstrap=100),
+                     axis=1)
+    reps.columns = np.arange(reps.shape[1]) + 1
+
+    dav = pd.concat([reps.mean(axis=1), reps.std(axis=1)], axis=1)
+    dav.columns = ['mean', 'std']
+
+    def plot_average_frequencies(dav):
+        fig, ax = plt.subplots()
+        fs = 16
+        colors = {'B': 'darkorange', 'H': 'steelblue',
+                  'T': 'seagreen', 'X': 'black', '-': 'grey'}
+        lss = {True: '--', False: '-'}
+        d = {True: 'syn', False: 'nonsyn'}
+        labs = {'B': 'sheet', 'T': 'turn',
+                'H': 'helix', 'X': 'unstructured'}
+        for (syn, sec_str), datum in dav.unstack('time_binc').iterrows():
+            datum = datum.unstack().T
+            x = np.array(datum.index)
+            y = np.array(datum['mean'])
+            yerr = np.array(datum['std'])
+            ax.errorbar(x, y,
+                        yerr=yerr,
+                        lw=3, ls=lss[syn], color=colors[sec_str],
+                        label=d[syn]+', '+labs[sec_str])
+        ax.set_xlim(0, 2600)
+        ax.set_ylim(0, 0.015)
+        ax.legend(loc='upper left', ncol=2, fontsize=fs-1)
+        ax.set_xlabel('Days since EDI', fontsize=fs)
+        ax.set_ylabel('Allele frequency', fontsize=fs)
+        ax.xaxis.set_tick_params(labelsize=fs)
+        ax.yaxis.set_tick_params(labelsize=fs)
+
+        plt.tight_layout()
+        plt.ion()
+        plt.show()
