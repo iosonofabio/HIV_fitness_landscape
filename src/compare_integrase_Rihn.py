3249f78d8a8066975a3d2bc2246b7353792a425f
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2016-05-27 21:58:07 +0200

    Comparison script works

--- src/compare_integrase_Rihn.py
+++ src/compare_integrase_Rihn.py
@@ -12,6 +12,7 @@ import sys
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
+from Bio.Seq import translate
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -32,7 +33,8 @@ def get_integrase_Rihn():
     '''Get the integrase sequence from Rihn'''
     seq = get_plasmid_Rihn()
 
-    # I aligned to HXB2, there are no indels
+    # I aligned to HXB2 or NL4-3, there are no indels
+    # That is no surprise as their plasmid is an HXB2/NL4-3 recombinant
     #reference = HIVreference(refname='HXB2', subtype='B', load_alignment=False)
     #ref_in = reference.annotation['IN'].extract(reference.seq)
 
@@ -55,7 +57,85 @@ def load_costs_Rihn():
            )
     df['rep'] /= 100.
     df['cost'] = np.maximum(0, 1.0 - df['rep'])
-    return df[['mut', 'cost']].set_index('mut')
+    df['pos'] = [int(it[1:-1]) for it in df['mut']]
+    df['NL4-3'] = [it[0] for it in df['mut']]
+    df['nuc'] = [it[-1] for it in df['mut']]
+    return df[['pos', 'NL4-3', 'nuc', 'mut', 'cost']].sort_values('pos')
+
+
+def load_costs_ours():
+    '''Load the fitness costs from us'''
+    fn = 'data/pol_selection_coefficients_st_B.tsv'
+    df = (pd.read_csv(fn, sep='\t', header=1)
+          .rename(columns={'# position': 'pos'}))
+    ref = ''.join(df['NL4-3'])
+
+    # Cut out only the integrase
+    ref = ref[715: 715 + 289]
+    df = df.iloc[715: 715 + 289]
+    df.index -= 715
+    df.pos -= 715
+
+    return ref, df
+
+
+def find_Rihn_multiple_alleles(costs):
+    '''Find the positions for which Rihn has multiple alleles'''
+    d = {key: value for key, value in Counter(costs_Rihn['pos']).iteritems() if value >= 2}
+    return costs.loc[costs['pos'].isin(d.keys())]
+
+
+def get_our_costs_at_Rihn(costs_Rihn, costs_ours):
+    '''Get our costs at their positions'''
+    c = costs_ours.set_index('pos').loc[costs_Rihn['pos']]['median']
+    c_float = []
+    for ci in c:
+        if ci == '<0.001':
+            ci = 0
+        elif ci == '>0.1':
+            ci = 1
+        else:
+            ci = float(ci)
+        c_float.append(ci)
+    c[:] = c_float
+
+    comp = (pd.concat([c, costs_Rihn.set_index('pos')['cost']], axis=1)
+            .rename(columns={'median': 'ours', 'cost': 'Rihn'}))
+
+    return comp
+
+
+def plot_comparison(comp):
+    '''Plot comparison of us with Rihn'''
+    fig, ax = plt.subplots(figsize=(5, 5))
+
+    from scipy.stats import spearmanr
+    s = spearmanr(comp['ours'], comp['Rihn'])
+    print(s)
+
+    shuffled_Rihn = np.array(comp['Rihn'])
+    for i in xrange(5):
+        np.random.shuffle(shuffled_Rihn)
+        s_shuffled = spearmanr(np.array(comp['ours']), shuffled_Rihn)
+        print('Shuffle, try', i, s_shuffled)
+
+    label = 'rho = '+str(s[0])[:4]+'\n'+'P = '+str(s[1])[:5]
+    ax.scatter(comp['ours'] + 0.0001, comp['Rihn'] + 0.0001,
+               s=40, color='k',
+               label=label)
+    ax.set_xlabel('Our cost')
+    ax.set_ylabel('Cost from Rihn et al. 2015')
+
+    ax.set_xlim(0.0001, 1.01)
+    ax.set_ylim(0.0001, 1.01)
+    ax.set_xscale('log')
+    ax.text(1.2e-4, 0.7, label, fontsize=12)
+
+    plt.tight_layout()
+    plt.ion()
+    plt.show()
+
+    return fig
 
 
 
@@ -63,4 +143,11 @@ def load_costs_Rihn():
 if __name__ == '__main__':
 
     seq = get_integrase_Rihn()
-    costs = load_costs_Rihn()
+    costs_Rihn = load_costs_Rihn()
+    ref, costs_ours = load_costs_ours()
+
+    comp = get_our_costs_at_Rihn(costs_Rihn, costs_ours)
+
+    fig = plot_comparison(comp)
+    for ext in ['svg', 'pdf', 'png']:
+        fig.savefig('figures/comparison_integrase_Rihn2015.'+ext)
